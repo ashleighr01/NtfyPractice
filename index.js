789948b24cec3b77067c5e960959d976b3cf33c5
ashleighr01 <ashleighr01@gmail.com>
2023-04-08 23:05:01 -0500

    Adjust some pieces

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 fetch('https://ntfy.sh/arufus_test', {
     method: 'POST', // PUT works too
-    headers: {"Title" : "Check the senior website!"},
-    body: `There's a new scholarship available!`,
+    body: "There's a new scholarship available!",
+    headers: {
+        "Title" : "Check the senior website!", 
+        "Priority" : "4"
+    }
 })
