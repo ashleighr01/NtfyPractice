b716f0001567b3352c752cf66db9a215a4d23440
ashleighr01 <ashleighr01@gmail.com>
2023-04-08 22:36:33 -0500

    Add a title

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 fetch('https://ntfy.sh/arufus_test', {
     method: 'POST', // PUT works too
-    headers: {
-        
-    },
+    headers: {"Title" : "Check the senior website!"},
     body: `There's a new scholarship available!`,
 })
