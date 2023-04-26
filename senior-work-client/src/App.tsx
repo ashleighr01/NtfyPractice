e0373cab8fb5b26ed193e08dd74739296d1e3221
ashleighr01 <ashleighr01@gmail.com>
2023-04-26 17:11:02 -0500

    Fix menu layout

--- senior-work-client/src/App.tsx
+++ senior-work-client/src/App.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
-  const [message, setMessage] = React.useState('This is the default message')
-  
-  fetch('http://localhost:8000').then((response)=> {
-      response.json().then((actualResponse) => {
-        console.log(actualResponse)
-        setMessage(actualResponse.message)
-      });
+  const [message, setMessage] = React.useState("This is the default message");
+
+  fetch("http://localhost:8000").then((response) => {
+    response.json().then((actualResponse) => {
+      console.log(actualResponse);
+      setMessage(actualResponse.message);
+    });
   });
   return (
     <div className="App">
-      <div id="menu">
-        <p>Menu</p>
-        <p>Info</p>
+      <div id="container">
+        <div id="menu">
+          <p>Menu</p>
+          <p>Info</p>
+        </div>
+        <header className="App-header">
+          <h3>Here is another written piece</h3>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            className="graduationphoto"
+            alt="graduation"
+            src="https://thehill.com/wp-content/uploads/sites/2/2022/03/ca_grad_diploma.jpg?w=960&h=540&crop=1"
+          />
+          <a
+            className="App-link"
+            href="https://google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {message}
+            <h1>Will this be seen?</h1>
+          </a>
+        </header>
       </div>
-      <header className="App-header">
-        <h3>Here is another written piece</h3>
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fthehill.com%2Fopinion%2Feducation%2F3491903-the-pandemic-changed-the-plans-of-many-2022-high-school-graduates%2F&psig=AOvVaw3vzQcWdRHC-qB--wvaLoJ5&ust=1682292136896000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKj00dzQvv4CFQAAAAAdAAAAABAE' />
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          {message}
-          <h1>Will this be seen?</h1>
-      
-        </a>
-      </header>
-      
-      
-
     </div>
   );
 }
